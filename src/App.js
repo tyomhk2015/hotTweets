3fd354ffd47fe0812de7b4b1fb3bd6578a12fe08
tyomhk <cheertrain15@naver.com>
2021-12-11 20:22:31 +0900

    Added initial CRA settings.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hello!
+      </h1>
+      <Button text="HELP!!" />
+    </div>
+  );
+}
+
+export default App;
