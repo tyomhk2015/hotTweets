de56b22d11b81f3704f9027c02bcf06be64f483c
tyomhk <cheertrain15@naver.com>
2021-12-12 00:00:41 +0900

    Tested module css

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,18 @@
+import propTypes from "prop-types"
 import React from 'react'
+import style from './Button.module.css'
 
-const Button = (props) => {
+const Button = ({text}) => {
   return (
-    <button>
-      {props.text}
+    <button className={style.btn}>
+      {text}
     </button>
   )
+}
+
+// Set prop types.
+Button.propTypes = {
+  text: propTypes.string.isRequired
 }
 
 export default Button
