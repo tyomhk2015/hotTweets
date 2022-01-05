ca13be673556787e1f474b6851549935c92bd1ff
tyomhk <cheertrain15@naver.com>
2022-01-06 00:21:33 +0900

    Created logout w/ firebase authentication, a global navigation component.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Home () {
+function Home() {
+  const [post, setPost] = useState('');
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(post);
+  }
+  const onChange = (event) => {
+    const {target: {value}} = event;
+    setPost(value);
+  }
   return (
-    <div>
-      Home
-    </div>
+    <form onSubmit={onSubmit}>
+      <input type='text' placeholder='Speak your mind!' maxLength={300} onChange={onChange}/>
+      <input type='submit' value='Post'/>
+    </form>
   );
 }
 
