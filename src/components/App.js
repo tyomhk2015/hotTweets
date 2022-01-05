6280f8265a161d89285df229d02ee2b0b8efc6af
tyomhk <cheertrain15@naver.com>
2022-01-05 21:20:11 +0900

    Successfully connected to firebase authentication.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AppRouter from 'components/Router';
-import {authService} from '../firebase_assets';
+import { authService } from '../firebase_assets';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  console.log(authService.currentUser);
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn}/>
+      {init ? <AppRouter isLoggedIn={isLoggedIn}/> : <p>Loading...</p>}
       <footer>&copy; Tyomhk</footer>
     </>
   );
