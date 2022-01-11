412463a5667549281eefa0467a2212f5547fd084
tyomhk <cheertrain15@naver.com>
2022-01-11 17:46:37 +0900

    Enabled changing nickname from navigation component.

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,35 @@ import { authService } from '../firebase_assets';
 function App() {
   const [init, setInit] = useState(false);
   const [user, setUser] = useState(null);
+
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setUser(user);
-        console.log(user.displayName);
+        // Create a simplified user object for React to track the objects' difference, so the component can be re-rendered.
+        const userObj = {
+          displayName: user.displayName,
+          uid: user.id,
+          updateProfile: (args) => user.updateProfile(args),
+        };
+        setUser(userObj);
       }
       setInit(true);
     });
   }, []);
+
+  const refreshUser = () => {
+    const retrievedUser = authService.currentUser;
+    const userObj = {
+      displayName: retrievedUser.displayName,
+      uid: retrievedUser.id,
+      updateProfile: (args) => retrievedUser.updateProfile(args),
+    };
+    setUser(userObj);
+  }
+
   return (
     <>
-      {init ? <AppRouter isLoggedIn={Boolean(user)} user={user} /> : <p>Loading...</p>}
+      {init ? <AppRouter isLoggedIn={Boolean(user)} user={user} refreshUser={refreshUser}/> : <p>Loading...</p>}
     </>
   );
 }
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,18 +1,62 @@
+import React, { useState } from 'react';
 import { authService } from '../firebase_assets';
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function Profile () {
+function Profile({ user, refreshUser }) {
+  const [ displayName, setDisplayName ] = useState(user.displayName);
   const navigate = useNavigate();
   const onLogOutClick = () => {
     authService.signOut();
     navigate('/');
-  }
+  };
+
+  const updateUserName = async (event) => {
+    event.preventDefault();
+    if(displayName !== user.displayName) {
+      await user.updateProfile({
+        displayName: displayName,
+      });
+      refreshUser();
+    }
+  };
+
+  const changeName = (event) => {
+    const { target : {value} } = event;
+    setDisplayName(value);
+  };
+
   return (
     <>
+      <form onSubmit={updateUserName}>
+        <input
+          type='text'
+          placeholder='Display Name'
+          onChange={changeName}
+          value={displayName}
+          required
+        />
+        <input type='submit' value='Update the name' />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
 }
 
 export default Profile;
+
+
+// const getMyPosts = async () => {
+//   const retrievedPosts = await dbService
+//     .collection('posts')
+//     .where('creator', '==', user.uid)
+//     .orderBy('createdDate')
+//     .get();
+//   retrievedPosts.docs.map((doc) => {
+//     console.log(doc.data());
+//     return null;
+//   });
+// };
+
+// useEffect(() => {
+//   getMyPosts();
+// }, []);
